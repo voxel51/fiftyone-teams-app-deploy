2b6d0b5f319abd23e9312324e4c0bba52d9f6d75
Alexander Foley <alex@voxel51.com>
2024-10-18 10:04:44 -0400

    chore: Remove extraneous function

--- tests/integration/helm/common_test.go
+++ tests/integration/helm/common_test.go
@@ -232,7 +232,7 @@ func checkPodLogsWithRetries(subT *testing.T, kubectlOptions *k8s.KubectlOptions
 
 		for i := 0; i < maxRetries; i++ {
 			log = get_logs(subT, kubectlOptions, &pod, "")
-			if contains(log, expected) {
+			if strings.Contains(s, substr)(log, expected) {
 				// Log entry found, proceed to next pod
 				break
 			}
@@ -245,13 +245,9 @@ func checkPodLogsWithRetries(subT *testing.T, kubectlOptions *k8s.KubectlOptions
 
 		// Final assertion
 		subT.Run(fmt.Sprintf("%s - %s", tc, svc), func(t *testing.T) {
-			if !contains(log, expected) {
+			if !strings.Contains(s, substr)(log, expected) {
 				t.Errorf("[ERROR]: %s - %s - log should contain matching entry:\n\t%s", tc, svc, expected)
 			}
 		})
 	}
-}
-
-func contains(s, substr string) bool {
-	return strings.Contains(s, substr)
 }
