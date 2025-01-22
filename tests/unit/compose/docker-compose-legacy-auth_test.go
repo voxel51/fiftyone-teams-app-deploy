d7f71d6099e0d08752e5dc537c2645e3911907a7
afoley587 <54959686+afoley587@users.noreply.github.com>
2025-01-22 16:17:17 -0500

    Update tests/unit/compose/docker-compose-legacy-auth_test.go
    Co-authored-by: topher <topher@voxel51.com>
    Signed-off-by: afoley587 <54959686+afoley587@users.noreply.github.com>

--- tests/unit/compose/docker-compose-legacy-auth_test.go
+++ tests/unit/compose/docker-compose-legacy-auth_test.go
@@ -185,7 +185,7 @@ func (s *commonServicesLegacyAuthDockerComposeTest) TestServiceImage() {
 			"teams-do",
 			[]string{legacyAuthComposeDelegatedOperationsFile},
 			s.dotEnvFiles,
-			"voxel51/fiftyone-teams-cv-full:v2.4.0",
+			"voxel51/fiftyone-teams-cv-full:v2.5.0",
 		},
 	}
 
