5ae19a4bf15d6310389251a932a5e7718006764b
prernadh <dhareshwar.prerna@gmail.com>
2026-09-24 10:29:37 -0700

    bump fiftyone-activity image refs and README example to 2.25.1

--- tests/unit/compose/docker-compose-legacy-auth_test.go
+++ tests/unit/compose/docker-compose-legacy-auth_test.go
@@ -261,7 +261,7 @@ func (s *commonServicesLegacyAuthDockerComposeTest) TestServiceImage() {
 			"activity-worker",
 			[]string{legacyAuthComposeFile, legacyAuthComposeActivityFile},
 			s.dotEnvFiles,
-			"voxel51/fiftyone-activity:v2.25.0",
+			"voxel51/fiftyone-activity:v2.25.1",
 		},
 		{
 			"defaultFiftyoneApp",
